4c04578b401ef2924dce8a941e590ed459e8d586
we.taper <cn19998@qq.com>
2016-08-16 13:24:02 +0800

    Update QFT in a nutshell, Complex Geometry notes. Improved git-push.bat script for visual echo.

--- git-push.bat
+++ git-push.bat
@@ -1,7 +1,12 @@
+echo off
 git status
+echo "Continue to git add & commit?"
 pause
 git add --all
 git commit -a
+echo "Continue to push?"
 pause
 git push origin master
+echo "Press any key to exit"
 pause
+exit
